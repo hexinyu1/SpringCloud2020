8e1c237e8c5369497c28fadbf0307b198ac153cf
Hexinyu111 <544995415@qq.com>
2020-06-09 14:36:05 +0800

    添加微服务支付模板

---
+++ cloud-provider-payment8001/src/main/java/com/xiaohe/cloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.xiaohe.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: XiaoHe
+ * @Date: 2020/6/9 14:32
+ * @Content:支付生产者启动类
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+
+}
